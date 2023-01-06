5ee525cede24ef17d86d946e5ab30731b2f0b367
Jadon Lee <91638482+JadonLee8@users.noreply.github.com>
2023-01-05 20:33:26 -0600

    updated some stuff

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -1,8 +1,8 @@
 export default {
-	title: `Odyssey Astro Theme | A Marketing Website Theme for Startups and Businesses`,
-	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
-	url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
-	name: `Odyssey`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	title: `Westwood Game Development Club`,
+	description: `The Official Website of the Westwood Game Development Club.`,
+	url: `https://WestwoodGameDev.github.io`, // No trailing slash!
+	name: `WWGameDev`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: true,
   showLittleSticksPlug: true, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
 };
