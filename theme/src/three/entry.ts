e6394a741eba7372705fbc6926d1c8f8379bc4d6
Alex Bui <zanbowie138@gmail.com>
2023-06-14 19:31:14 -0500

    disable canvas on mobile

--- theme/src/three/entry.ts
+++ theme/src/three/entry.ts
@@ -1,8 +1,13 @@
 import {TCanvas} from './TCanvas'
 import {qs} from './utils'
 
-const m_canvas = new TCanvas(qs<HTMLDivElement>('.webgl-canvas__container'));
+// Only create if not a mobile device
+if(!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+  const m_canvas = new TCanvas(qs<HTMLDivElement>('.webgl-canvas__container'));
 
-window.addEventListener('beforeunload', () => {
+  window.addEventListener('beforeunload', () => {
     m_canvas.dispose()
   })
+} else {
+  console.log('Mobile device detected! WebGLCanvas not created.')
+}
